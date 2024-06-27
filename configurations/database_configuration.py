215282306f3d9083557f0c3fb21c9198ec92140c
Hypercodding <mohammadhammad302@gmail.com>
2024-06-27 15:49:42 +0500

    commit

--- configurations/database_configuration.py
+++ configurations/database_configuration.py
@@ -1,5 +1,6 @@
 class Settings:
     PROJECT_NAME: str = "FastAPI Project"
-    SQLALCHEMY_DATABASE_URL: str = "postgresql://postgres:Hyper@localhost/Alchemy"
+    SQLALCHEMY_DATABASE_URL: str = "postgresql://postgres:Hyper@192.168.4.164/Alchemy"
 
 settings = Settings()
+ 
